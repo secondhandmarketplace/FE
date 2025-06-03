ef09aba5700996bd6d6e3e4ac334dd31e3bf1e03
wltkdgur123@naver.com <wltkdgur123@naver.com>
2025-06-03 20:27:38 +0900

    기능 개발 완료 및 백엔드 연동

--- kdtprj2/src/main.jsx
+++ kdtprj2/src/main.jsx
@@ -1,28 +1,14 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import LoginRoutes from "./routes/loginRoutes.jsx";
-import homeRoutes from "./routes/homeRoutes.jsx";
-import ChatRoutes from "./routes/chatRoutes.jsx";
-import historyRoutes from "./routes/historyRoutes.jsx";
-import searchRoutes from "./routes/searchRoutes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          {LoginRoutes}
-          {homeRoutes}
-          {ChatRoutes}
-          {historyRoutes}
-          {searchRoutes}
-        </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
